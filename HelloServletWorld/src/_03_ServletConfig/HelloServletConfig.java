b1b22ef15d9a73eaabc25c1fbac744ffb62422ca
Karayel <mertkarayel94@hotmail.com>
2015-03-01 22:31:59 +0200

    Servlet Config

---
+++ HelloServletWorld/src/_03_ServletConfig/HelloServletConfig.java
@@ -0,0 +1,41 @@
+package _03_ServletConfig;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServletConfig extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		PrintWriter pw = resp.getWriter();
+		ServletConfig config = getServletConfig();
+
+		String driverName = config.getInitParameter("driverClassName");
+		String url = config.getInitParameter("url");
+		String name = config.getInitParameter("username");
+		String password = config.getInitParameter("password");
+
+		String servletName = config.getServletName();
+
+		pw.print("<html><body>");
+		pw.print(servletName + "<br>");
+		pw.print(driverName + "<br>");
+		pw.print(url + "<br>");
+		pw.print(name + "<br>");
+		pw.print(password + "<br>");
+		pw.print("</body></html>");
+
+		Enumeration<String> names = config.getInitParameterNames();
+		while(names.hasMoreElements()){
+			System.out.println(names.nextElement());
+		}
+	}
+}
