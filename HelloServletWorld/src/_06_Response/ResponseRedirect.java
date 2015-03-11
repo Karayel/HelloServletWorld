c1920acc9f6a4df842eea050d5b76a045df4d922
karayel <karayel@ubuntu>
2015-03-11 15:39:24 -0700

    ResponceRedirect

---
+++ HelloServletWorld/src/_06_Response/ResponseRedirect.java
@@ -0,0 +1,26 @@
+package _06_Response;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/responseredirect")
+public class ResponseRedirect extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		int status = HttpServletResponse.SC_MOVED_TEMPORARILY; // 301
+
+		if (status == HttpServletResponse.SC_OK) { // 200
+			// /....
+		} else {
+			resp.sendRedirect("http://www.injavawetrust.com");
+		}
+
+	}
+}
