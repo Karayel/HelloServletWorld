67a44cde1d0f90044822d755d84c6992838793ec
karayel <karayel@ubuntu>
2015-03-11 13:40:53 -0700

    Request

--- HelloServletWorld/src/_05_Request/MoreRequest.java
+++ HelloServletWorld/src/_05_Request/MoreRequest.java
@@ -18,14 +18,24 @@ public class MoreRequest extends HttpServlet {
 			throws ServletException, IOException {
 
 		PrintWriter pw = resp.getWriter();
-
+		// getHeader() metodu ile header bilgilerine ulasabiliriz.
 		String hostHeader = req.getHeader("host");
+		// getHeaderNames() metodu tum header bilgilerini geri dondururur.
 		Enumeration<String> hostHeaders = req.getHeaderNames();
+		// getServerName() metodu server ismini dondurur; localhost.
 		String serverName = req.getServerName();
+		/**
+		 * local port ve server port arasinda kucuk bir kavram farkliligi
+		 * vardir. server port requestin ilk karsilandigi basladigi porttur.
+		 * local port ise bu istegin tamamlandigi porttur.
+		 */
 		int localPort = req.getLocalPort();
 		int serverPort = req.getServerPort();
+		//Istegin gonderildigi porttur. Remote’tan kasit client/istemci yani tarayicidir.
 		int remotePort = req.getRemotePort();
+		//getMethod() , bize kullandigimiz HTTP metodunun tur bilgisini dondurur.
 		String method = req.getMethod();
+		//getContextPath() metodu context path bilgisini dondurecektir.
 		String contexPath = req.getContextPath();
 		Enumeration<String> headerNames = req.getHeaderNames();
 
@@ -47,7 +57,7 @@ public class MoreRequest extends HttpServlet {
 		pw.write("</table>");
 		pw.write("</body></html>");
 		pw.write("Host Headers ");
-		
+
 		while (hostHeaders.hasMoreElements()) {
 			pw.println(hostHeaders.nextElement());
 		}
